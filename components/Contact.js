1b03e1eab50addf26e620c0a654a34a736bd069f
Ibrahim <ibrahimnas1@yahoo.com>
2024-11-12 23:49:51 +0000

    initial commit

---
+++ components/Contact.js
@@ -0,0 +1,187 @@
+import React from "react";
+import userData from "@constants/data";
+
+export default function Contact() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800 antialiased">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          Contact
+        </h1>
+      </div>
+      <div className="relative z-10 rounded-md shadow-md bg-[#02044A] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="md:ml-4">
+            <header className="">
+              <h1 className="text-gray-50 font-semibold text-2xl">
+                Get in touch, let's talk.
+              </h1>
+              <p className="font-light text-base text-gray-200 mt-2">
+                Send me a message and I'll get back to you as soon as I can.
+              </p>
+            </header>
+            <div className="icons-container inline-flex flex-col my-20">
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-telephone-fill h-4 w-4 text-blue-500"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+                  />
+                </svg>
+                <p className="text-gray-50 font-light text-sm">
+                  {userData.phone}
+                </p>
+              </div>
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-envelope-fill h-4 w-4 text-blue-500"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
+                </svg>
+                <p className="text-gray-50 font-light text-sm">
+                <a 
+                  href={`mailto:${userData.email}`}
+                >{userData.email}</a>
+                </p>
+              </div>
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-pin-fill h-4 w-4 text-blue-500"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
+                </svg>
+                <p className="text-gray-50 font-light text-sm">
+                  {userData.address}
+                </p>
+              </div>
+            </div>
+            <div className="social-icons flex flex-row space-x-8">
+              <a
+                href={userData.socialLinks.github}
+                className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
+                target="_blank" rel="noopener noreferrer"
+              >
+                <svg
+                  width="30"
+                  height="30"
+                  className="text-gray-50"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                 >
+                  <path xmlns="http://www.w3.org/2000/svg" d="M9.29183 21V18.4407L9.3255 16.6219C9.36595 16.0561 9.58639 15.5228 9.94907 15.11C9.95438 15.1039 9.95972 15.0979 9.9651 15.0919C9.9791 15.0763 9.96988 15.0511 9.94907 15.0485V15.0485C7.52554 14.746 5.0005 13.7227 5.0005 9.26749C4.9847 8.17021 5.3427 7.10648 6.00437 6.27215C6.02752 6.24297 6.05103 6.21406 6.07492 6.18545V6.18545C6.10601 6.1482 6.11618 6.09772 6.10194 6.05134C6.10107 6.04853 6.10021 6.04571 6.09935 6.04289C6.0832 5.9899 6.06804 5.93666 6.05388 5.88321C5.81065 4.96474 5.86295 3.98363 6.20527 3.09818C6.20779 3.09164 6.21034 3.08511 6.2129 3.07858C6.22568 3.04599 6.25251 3.02108 6.28698 3.01493V3.01493C6.50189 2.97661 7.37036 2.92534 9.03298 4.07346C9.08473 4.10919 9.13724 4.14609 9.19053 4.18418V4.18418C9.22901 4.21168 9.27794 4.22011 9.32344 4.20716C9.32487 4.20675 9.32631 4.20634 9.32774 4.20593C9.41699 4.18056 9.50648 4.15649 9.59617 4.1337C11.1766 3.73226 12.8234 3.73226 14.4038 4.1337C14.4889 4.1553 14.5737 4.17807 14.6584 4.20199C14.6602 4.20252 14.6621 4.20304 14.6639 4.20356C14.7174 4.21872 14.7749 4.20882 14.8202 4.17653V4.17653C14.8698 4.14114 14.9187 4.10679 14.967 4.07346C16.6257 2.92776 17.4894 2.9764 17.7053 3.01469V3.01469C17.7404 3.02092 17.7678 3.04628 17.781 3.07946C17.7827 3.08373 17.7843 3.08799 17.786 3.09226C18.1341 3.97811 18.1894 4.96214 17.946 5.88321C17.9315 5.93811 17.9159 5.9928 17.8993 6.04723V6.04723C17.8843 6.09618 17.8951 6.14942 17.9278 6.18875C17.9289 6.18998 17.9299 6.19121 17.9309 6.19245C17.9528 6.21877 17.9744 6.24534 17.9956 6.27215C18.6573 7.10648 19.0153 8.17021 18.9995 9.26749C18.9995 13.747 16.4565 14.7435 14.0214 15.015V15.015C14.0073 15.0165 14.001 15.0334 14.0105 15.0439C14.0141 15.0479 14.0178 15.0519 14.0214 15.0559C14.2671 15.3296 14.4577 15.6544 14.5811 16.0103C14.7101 16.3824 14.7626 16.7797 14.7351 17.1754V21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+              </a>
+              <a
+                href={userData.socialLinks.twitter}
+                className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
+                  target="_blank" rel="noopener noreferrer"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  className="text-gray-50"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M8 3C9.10457 3 10 3.89543 10 5V8H16C17.1046 8 18 8.89543 18 10C18 11.1046 17.1046 12 16 12H10V14C10 15.6569 11.3431 17 13 17H16C17.1046 17 18 17.8954 18 19C18 20.1046 17.1046 21 16 21H13C9.13401 21 6 17.866 6 14V5C6 3.89543 6.89543 3 8 3Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </a>
+              <a
+                href={userData.socialLinks.instagram}
+                className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
+                  target="_blank" rel="noopener noreferrer"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  className="text-gray-50"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7ZM9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M5 1C2.79086 1 1 2.79086 1 5V19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5ZM19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          {/*<form className="form rounded-lg bg-white p-4 flex flex-col">
+            <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+              {" "}
+              Your Name
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="name"
+            />
+            <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
+              Email
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="email"
+            />
+            <label
+              htmlFor="message"
+              className="text-sm text-gray-600 mx-4 mt-4"
+            >
+              Message
+            </label>
+            <textarea
+              rows="4"
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="message"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
+            >
+              Send Message
+            </button>
+          </form>*/}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ constants/data.js
@@ -0,0 +1,90 @@
+const userData = {
+  githubUsername: "EbrahimNas",
+  name: "Ibrahim Nasir",
+  designation: "Software Developer",
+  avatarUrl: "/avatar1.png",
+  email: "ibrahimnas1@yahoo.com",
+  phone: "+44 7466796634",
+  address: "Birmingham, United Kingdom.",
+  projects: [
+    {
+      title: "NussyNas (E-commerce Web App)",
+      link: "https://nussynas.netlify.app/",
+      imgUrl: "/NN.png",
+    },
+    {
+      title: "Jammming",
+      link: "https://jammmingforspotify.netlify.app/",
+      imgUrl: "/jsp.png",
+    },
+    {
+      title: "Number Guesser - Game",
+      link: "https://number-guesser-game-enas.netlify.app",
+      imgUrl: "/nbg.png",
+    },
+    {
+      title: "Find your Hat - Game",
+      link: "https://github.com/EbrahimNas/find-your-hat-game",
+      imgUrl: "/fyh.png",
+    },
+    
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building  Cool Stuff, with Design Roots in Architecture",
+    description: [
+      `Coming from an architecture background, I bring a unique perspective to software development, where design, structure, and user experience converge. My journey from architecture to tech is fueled by a desire to bridge the gap between the digital and physical worlds, crafting solutions that are as intuitive as they are impactful.`,
+      `I specialize in building dynamic and scalable web applications using React.js, Next.js, TypeScript, and JavaScript, with a focus on clean design, efficiency, and seamless user experiences. My architectural insights inform every step, from wireframing and planning to crafting user-centric solutions that merge innovative design with functional technology.`,
+      `With hands-on experience in agile methodologies, code optimization, and effective collaboration across cross-functional teams, I’m skilled at rapidly learning new tools and solving complex technical challenges. My goal? To create software that integrates technology into the built environment, uniting diverse disciplines and enriching both digital and physical spaces. Whether it's through performance-driven front-end solutions or intuitive user interfaces, I’m here to make technology engaging, efficient, and designed for impact.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Assistant Project Manager",
+      company: "Tiege Consult.",
+      year: "Oct '22 - Present",
+      companyLink: "",
+      desc: "Collaborated closely with front-end developers, contributing to the development of reusable components in React.",
+    },
+    {
+      title: "Project Coordinator ",
+      company: "James Blake Associates.",
+      year: "Aug '23 - Sep '23",
+      companyLink: "",
+      desc: "Developed and maintained proactive relationships with clients, enhancing customer satisfaction and business growth.",
+    },
+    {
+      title: "Junior Project Manager",
+      company: "Alboss Consulting.",
+      year: "Jan '20 - Aug '20",
+      companyLink: "",
+      desc: "Assisted with software development project management, focusing on user stories and front-end architecture.",
+    },
+    {
+      title: "Junior Architect | Project Support Officer",
+      company: "Federal Housing Authority, Abuja",
+      year: "Nov '18 - Nov '19",
+      companyLink: "",
+      desc: "Prepared Architectural drawings and Visualisations using Computer Aided Design programs.",
+    },
+    {
+      title: "Architectural Assistant, Intern",
+      company: "Aim Consultant.",
+      year: "May '17 - Oct '17",
+      companyLink: "",
+      desc: "Prepared Architectural drawings and Visualisations using Computer Aided Design programs.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1uw6O_yCX4oJWSBZ-zbj3iNTNaYxum6pC/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/ebrahim_nas",
+    twitter: "https://twitter.com/ebrahim_nass",
+    linkedin: "https://www.linkedin.com/in/ibrahim-nasir-47b959163/",
+    github: "https://github.com/EbrahimNas",
+  },
+};
+
+export default userData;
